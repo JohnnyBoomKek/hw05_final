71f7778b51b3b5d8879ecb08c15afd2c5b3e2085
JohnnyBoomKek <summer1710@qq.com>
2020-04-10 03:27:40 +0800

    my forth commit

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    path("<username>/follow", views.profile_follow, name="profile_follow"), 
+    path("<username>/unfollow", views.profile_unfollow, name="profile_unfollow"),
     path("follow/", views.follow_index, name="follow_index"),
     path("", views.index, name="index"),
     path('group/<slug:slug>', views.group_posts),
@@ -14,8 +16,7 @@ urlpatterns = [
     path("<username>/<int:post_id>/edit/", views.post_edit, name="post_edit"),
     path("<username>/<int:post_id>/comment/", views.add_comment, name="add_comment"),
     path('delete/<comment_id>', views.delete_comment,name='delete_comment'),
-    path("<username>/follow", views.profile_follow, name="profile_follow"), 
-    path("<username>/unfollow", views.profile_unfollow, name="profile_unfollow"),
+    
 
 ]
 if settings.DEBUG:
