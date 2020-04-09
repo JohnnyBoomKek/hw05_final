5c89117f36a19936387475c74e300dd33cbfd763
JohnnyBoomKek <summer1710@qq.com>
2020-04-10 03:34:13 +0800

    my fifth commit

--- posts/urls.py
+++ posts/urls.py
@@ -4,10 +4,9 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    path("follow/", views.follow_index, name="follow_index"),
     path("<username>/follow", views.profile_follow, name="profile_follow"), 
     path("<username>/unfollow", views.profile_unfollow, name="profile_unfollow"),
-    path("follow/", views.follow_index, name="follow_index"),
-    path("", views.index, name="index"),
     path('group/<slug:slug>', views.group_posts),
     path('search/', views.search, name='search'),
     path('new/', views.new, name='new'),
@@ -16,6 +15,7 @@ urlpatterns = [
     path("<username>/<int:post_id>/edit/", views.post_edit, name="post_edit"),
     path("<username>/<int:post_id>/comment/", views.add_comment, name="add_comment"),
     path('delete/<comment_id>', views.delete_comment,name='delete_comment'),
+    path("", views.index, name="index"),
     
 
 ]
