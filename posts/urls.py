e96795f3d2d09aee40b2c67b5feb70542821c0b8
JohnnyBoomKek <summer1710@qq.com>
2020-04-10 03:26:10 +0800

    my third commit

--- posts/urls.py
+++ posts/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path("<username>/<int:post_id>/edit/", views.post_edit, name="post_edit"),
     path("<username>/<int:post_id>/comment/", views.add_comment, name="add_comment"),
     path('delete/<comment_id>', views.delete_comment,name='delete_comment'),
-    path("<username>/follow/", views.profile_follow, name="profile_follow"), 
-    path("<username>/unfollow/", views.profile_unfollow, name="profile_unfollow"),
+    path("<username>/follow", views.profile_follow, name="profile_follow"), 
+    path("<username>/unfollow", views.profile_unfollow, name="profile_unfollow"),
 
 ]
 if settings.DEBUG:
